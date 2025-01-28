d7fd56ec96be5e31a0752e1ea2a4dbbbdd2f41d0
thangnd05 <thangnd.contact@gmail.com>
2025-01-28 13:27:45 +0700

    update

--- test/test/src/main/java/com/example/test/TestApplication.java
+++ test/test/src/main/java/com/example/test/TestApplication.java
@@ -3,9 +3,7 @@ package com.example.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(exclude = {
-//		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,}
-//)
+
 @SpringBootApplication
 public class TestApplication {
 
